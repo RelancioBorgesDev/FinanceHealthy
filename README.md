a4f0f1099fee3133a3fa574ed45c509dd169c09a
RelancioBorgesDev <relancio.dev@gmail.com>
2022-03-06 22:18:16 -0300

    README Atualizado

---
+++ README.md
@@ -0,0 +1,19 @@
+# Projeto - Finance Healthy 💸💰
+## Objetivo:
+- Praticar os conceitos básicos de CSS (position - display)
+- Manipular Inputs utilizando ReactJS
+
+
+
+# Linha do Tempo
+## Inicio: 06/03/2022
+
+**Cadastro Inicio:**
+![](/src/assets/Cadastro%20Inicio.png)
+
+**Cadastro Responsivo:**
+![](/src/assets/CadastroResponsivo.png)
+
+
+
+***🔥RelancioBorges🔥***
--- src/components/ButtonSwitchPage/ButtonSwitchPage.jsx
+++ src/components/ButtonSwitchPage/ButtonSwitchPage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import { BiLastPage } from 'react-icons/bi'
 import button from './button.module.css'
@@ -6,7 +7,7 @@ export default function ButtonSwitchPage() {
   return (
     <button className={button.btn}>
         <a href="#">
-        <BiLastPage className={button.btn_icon}/>
+          <BiLastPage className={button.btn_icon}/>
         </a>
   </button>
 
